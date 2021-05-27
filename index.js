fb478eb6f1bc0e0002e454171d584b6a96ee179c
Valentin Gallardo <valengallardo2@gmail.com>
2021-05-27 11:26:55 -0300

    Servidor corriendo en el puerto 3000, agregado Hola mundo como responsive de express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+})
+
+app.get("/", function(req, res) {
+    res.send("Hola Mundo");
+})
