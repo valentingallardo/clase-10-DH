281c39a2d211883b891ec7ba80fcd11f30213ec3
Valentin Gallardo <valengallardo2@gmail.com>
2021-05-27 15:20:51 -0300

    Agregado nodemon, pantalla /gif y principal.html

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const path = require("path");
+
+
+
 const app = express();
 
 app.listen(3000, () => {
@@ -8,3 +12,9 @@ app.listen(3000, () => {
 app.get("/", function(req, res) {
     res.send("Hola Mundo");
 })
+
+app.get("/gif", function(req, res){
+    let htmlPath = path.join(__dirname, "/public/img/gif1.gif");
+    res.sendFile(htmlPath);
+})
+
